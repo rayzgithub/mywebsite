10e556c20c3ada88c4a973487fe20dbfd5677068
zhouyong <zhouyong@weijinzaixian.com>
2018-07-13 14:56:33 +0800

    edit

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -10,12 +10,5 @@
 
 ?>
 <div class="site-info">
-	<?php
-	if ( function_exists( 'the_privacy_policy_link' ) ) {
-		the_privacy_policy_link( '', '<span role="separator" aria-hidden="true"></span>' );
-	}
-	?>
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>" class="imprint">
-		<?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?>
-	</a>
+	<a href="<?php echo home_url(); ?>">红尘作伴，共赴青春！</a>
 </div><!-- .site-info -->
--- wp-content/themes/twentyseventeen/template-parts/post/content.php
+++ wp-content/themes/twentyseventeen/template-parts/post/content.php
@@ -52,16 +52,15 @@
 	<div class="entry-content">
 		<?php
 		/* translators: %s: Name of current post */
-//		the_content( sprintf(
-//			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
-//			get_the_title()
-//		) );
-
-		if(!is_single()) {
-            the_excerpt();
-        } else {
-            the_content(__('(more…)'));
-        }
+// 		the_content( sprintf(
+// 			__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentyseventeen' ),
+// 			get_the_title()
+// 		) );
+ 		if(!is_single()) {
+			the_excerpt();
+      } else {
+        the_content(__('(more…)'));
+      }
 
 		wp_link_pages( array(
 			'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
